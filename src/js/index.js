ad0e8d82347e49e67501fc0cad92274585b16223
Arm=Band <bre4tn.let@gmail.com>
2018-08-10 23:06:57 +0900

    v_3.6.1-4.1.3 `index.js`のページ内スクロールの処理の書き方を変更

--- src/js/index.js
+++ src/js/index.js
@@ -27,22 +27,22 @@ function mobileSafariRequiem() {
 function pageTop() {
     var returnPageTop = $(".returnPageTop");
 
-	var startPos = 0;
-	$(window).on("scroll", function(){
+    var startPos = 0;
+    $(window).on("scroll", function(){
         //スクロール距離が400pxより大きければページトップへ戻るボタンを表示
-		var currentPos = $(this).scrollTop();
-		if (currentPos > 400) {
-			returnPageTop.fadeIn();
-		} else {
-			returnPageTop.fadeOut();
-		}
-	});
+        var currentPos = $(this).scrollTop();
+        if (currentPos > 400) {
+            returnPageTop.fadeIn();
+        } else {
+            returnPageTop.fadeOut();
+        }
+    });
 
-	//ページトップへスクロールして戻る
-	returnPageTop.on("click", function () {
-		$("body, html").animate({ scrollTop: 0 }, 1000, "easeInOutCirc");
-		return false;
-	});
+    //ページトップへスクロールして戻る
+    returnPageTop.on("click", function () {
+        $("body, html").animate({ scrollTop: 0 }, 1000, "easeInOutCirc");
+        return false;
+    });
 }
 
 //ページ内スクロール
@@ -53,7 +53,17 @@ function pageScroll() {
         $navbar.find("a").on("click", function() {
             var speed = 1000;
             var href = $(this).attr("href");
-            var target = $(href == "#" || href == "" ? "html" : href);
+            var targetID = "";
+            if(/^(\.\/|\/)$|^(#)?$/.test(href)) { //hrefの値が「/」「./」「#」「」の場合
+                targetID = "html";
+            }
+            else if(/^(\.\/|\/)#.+/.test(href)) { //hrefの値が「/#HOGE」「./#HOGE」「#HOGE」の場合
+                targetID = href.slice(RegExp.$1.length); //正規表現の後方参照により"(\.\/|\/)"をRegExp.$1に格納、その文字列の長さを削除し、「#HOGE」だけの状態にして渡す
+            }
+            else {
+                targetID = href;
+            }
+            var target = $(targetID);
             var position = target.offset().top - navbarHeight;
             $("body, html").animate({ scrollTop:position }, speed, "easeInOutCirc");
             $navbar.find(".navbar-toggle[data-target=\"#navbarMenu\"]").click(); //移動したらハンバーガーを折りたたむ
