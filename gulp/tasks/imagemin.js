5a27d08b3d9a105b4faf4bc052e0a2902662b671
Arm=Band <bre4tn.let@gmail.com>
2019-10-04 23:38:08 +0900

    - npmパッケージのアップデート - 画像圧縮タスクを改修     - 圧縮プラグインを追加     - `bin/config/plugin.yml`に画像圧縮か、ただのファイルコピーを選択するオプションフラグを追加 - フッタのコピーライト表記を簡略化

--- gulp/tasks/imagemin.js
+++ gulp/tasks/imagemin.js
@@ -1,9 +1,41 @@
 const _         = require('../plugin')
 const dir       = require('../dir')
+const functions = require('../functions')
+const plugins = functions.getConfig(dir.config.plugins)
+const IMGDIR = { src: `${dir.src.img}/**/*.+(jpg|jpeg|png|gif|svg)`, dist: dir.dist.img }
+const IMGPROC = { MINIFY: 'imageminify', COPY: 'imagecopy' }
+
+//フラグで画像に対する処理を分岐
+let IMAGEPROC
+if(plugins.imagemin) {
+    IMAGEPROC = IMGPROC.MINIFY
+}
+else {
+    IMAGEPROC = IMGPROC.COPY
+}
 
 //画像圧縮
-_.gulp.task('imagemin', () => {
-    return _.gulp.src(`${dir.src.img}/**/*.+(jpg|jpeg|png|gif|svg)`)
-        .pipe(_.imagemin())
-        .pipe(_.gulp.dest(dir.dist.img))
+_.gulp.task(IMGPROC.MINIFY, () => {
+    return _.gulp.src(IMGDIR.src)
+        .pipe(_.imagemin([
+            _.imageminPng({
+                quality: [0.80, 0.90],
+                speed: 1
+            }),
+            _.imageminJpeg({
+                quality: 90
+            }),
+            _.imageminSvg(),
+            _.imageminGif()
+          ]))
+        .pipe(_.gulp.dest(IMGDIR.dist))
 })
+//画像コピー(ファイルコピーのみ)
+_.gulp.task(IMGPROC.COPY, () => {
+    return _.gulp.src(IMGDIR.src)
+    .pipe(_.plumber())
+    .pipe(_.gulp.dest(IMGDIR.dist))
+})
+
+//コールタスク
+_.gulp.task('imagemin', _.gulp.parallel(IMAGEPROC))
