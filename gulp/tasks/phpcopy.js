964bb45ed17a8815a4e2268b5362240b865072ad
Arm=Band <bre4tn.let@gmail.com>
2019-12-11 23:58:16 +0900

    - PHPコピータスクから`composer`関連など不要なファイルをコピーしないように調整 - プラグインにScssで`font-size`, `line-height`, `margin`, `padding`のベースを追加するテンプレを追加

--- gulp/tasks/phpcopy.js
+++ gulp/tasks/phpcopy.js
@@ -4,7 +4,7 @@ const dir       = require('../dir');
 //phpcopy
 const phpcopy = () => {
     return _.gulp.src(
-        [`${dir.src.php}/**/*`, `!${dir.src.php}/.gitkeep`]
+        [`${dir.src.php}/**/*`, `!${dir.src.php}/.gitkeep`, `!${dir.src.php}/**/composer.json`, `!${dir.src.php}/**/*.md`, `!${dir.src.php}/**/composer.lock`]
     )
     .pipe(_.plumber())
     .pipe(_.gulp.dest(dir.dist.html));
