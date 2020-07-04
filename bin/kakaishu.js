602ae800399adc360969dfb66b03f997edff2ace
arm-band <bre4tn.let@gmail.com>
2020-07-04 23:57:44 +0900

    - `_honoka.scss`が存在している場合は上書きコピーしないように処理を変更(`zanmai`) - `kakaishu`も同様に`.env`の存在チェックを追加

--- bin/kakaishu.js
+++ bin/kakaishu.js
@@ -1,9 +1,10 @@
 const fs           = require('fs');
 const functions    = require('../gulp/functions');
 const sampleEnvFile = './sample.env';
+const envFile = './.env';
 
-if(functions.isExistFile(sampleEnvFile)) {
-    fs.writeFileSync('./.env', fs.readFileSync(sampleEnvFile, 'utf8'), (err) => {
+if(functions.isExistFile(sampleEnvFile) && !functions.isExistFile(envFile)) {
+    fs.writeFileSync(envFile, fs.readFileSync(sampleEnvFile, 'utf8'), (err) => {
         if(err) {
             console.log(err);
         }
---
+++ bin/zanmai.js
@@ -0,0 +1,14 @@
+const fs           = require('fs');
+const functions    = require('../gulp/functions');
+const dir    = require('../gulp/dir');
+const honokaPath = './node_modules/bootstrap-honoka/scss/honoka/';
+const scssAssetsPath = '/bootstrap/honoka/';
+const honokaFile = '_honoka.scss';
+
+if(functions.isExistFile(`${honokaPath}${honokaFile}`) && !functions.isExistFile(`${dir.src.scss}${dir.src.assets}${scssAssetsPath}${honokaFile}`)) {
+    fs.writeFileSync(`${dir.src.scss}${dir.src.assets}${scssAssetsPath}${honokaFile}`, fs.readFileSync(`${honokaPath}${honokaFile}`, 'utf8'), (err) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+}
