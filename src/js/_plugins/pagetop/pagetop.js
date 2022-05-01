b5472943bfeb0a36c987f7aaed3f5fd18e2d9d3e
arm-band <bre4tn.let@gmail.com>
2022-05-01 22:06:54 +0900

    - アンカーリンク遷移周りを大きく変更     - Bootstrap 5 で css の `scroll-behavior: smooth;` による遷移に変更となったので、 jQuery によるアンカーリンク遷移の自前実装を削除     - ハンバーガーメニュー内のアンカーリンク遷移時にハンバーガーメニューを閉じる動作を書き換え、プレーンJSによるものに置換     - ページトップへ戻るボタンも上述の仕様変更に合わせて処理を大幅に変更

--- src/js/_plugins/pagetop/pagetop.js
+++ src/js/_plugins/pagetop/pagetop.js
@@ -1,22 +1,83 @@
-import $ from 'jquery';
-
-//ページトップへ戻る
-export default (screlm) => {
-    const $returnPageTop = $('.returnPageTop');
-
-    $(window).on('scroll', function () {
-        //スクロール距離が400pxより大きければページトップへ戻るボタンを表示
-        let currentPos = $(this).scrollTop();
-        if (currentPos > 400) {
-            $returnPageTop.fadeIn();
-        } else {
-            $returnPageTop.fadeOut();
+/**
+ * ページトップへ戻る
+ *
+ * constructor:
+ *
+ * @param {string} observeSelectorName - 監視対象要素のセレクタ名 (デフォルト: .l-main)
+ * @param {string} targetSelectorName  - クラス付与要素のセレクタ名 (デフォルト: .c-returnPageTop)
+ */
+ class returnPageTop {
+    /**
+     * constructor
+     *
+     * @param {string} observeSelectorName - 監視対象要素のセレクタ名 (デフォルト: .l-main)
+     * @param {string} targetSelectorName  - クラス付与要素のセレクタ名 (デフォルト: .c-returnPageTop)
+     */
+    constructor(observeSelectorName = '.c-intersectionReturnPageTop', targetSelectorName = '.c-returnPageTop') {
+        // 監視対象要素
+        this.observeElms = document.querySelectorAll(observeSelectorName);
+        // DOM to Array
+        this.observeElmsArray = Array.prototype.slice.call(this.observeElms, 0);
+        // クラス付与要素
+        this.targetElms = document.querySelectorAll(targetSelectorName);
+        // クラス付与要素の Array
+        this.targetElmsArray = Array.prototype.slice.call(this.targetElms, 0);
+        // options
+        this.options = {
+            root: null,
+            rootMargin: '0px 0px -12%',
+            threshold: 0
+        };
+        // ブラウザの高さ
+        this.clientHeight = document.documentElement.clientHeight;
+        // ページトップへ戻る
+        this.rptObserver = this.rptShow(this.clientHeight);
+        window.addEventListener('resize', () => {
+            // resize でブラウザの表示領域の高さが変動したら
+            this.clientHeight = document.documentElement.clientHeight;
+            // 一旦監視を止める
+            this.rptObserver.disconnect();
+            // 再度監視
+            this.rptObserver = this.rptShow(this.clientHeight);
+        });
+    }
+    /**
+     * IntersectionObserver で指定するコールバック関数。ページトップへ戻るのクリック対象要素を、 targetSelectorName で指定した要素と交差するかを判定して、その結果で表示・非表示を切り替えるための .activeクラス の着脱を行う
+     *
+     * @param {Array} - elms
+     */
+    addClass = (elms) => {
+        const elmsArray = Array.prototype.slice.call(elms, 0);
+        for (const elm of elmsArray) {
+            // ブラウザ表示領域に対する対象要素の位置
+            const elmRectCoor = elm.target.getBoundingClientRect();
+            if ( 0 > elmRectCoor.bottom ) {
+                // ブラウザ表示領域に対する対象要素の上端の位置 が ブラウザの表示領域 より上
+                for (const targetElm of this.targetElmsArray) {
+                    targetElm.classList.add('active');
+                }
+            }
+            else {
+                for (const targetElm of this.targetElmsArray) {
+                    targetElm.classList.remove('active');
+                }
+            }
         }
-    });
-
-    //ページトップへスクロールして戻る
-    $returnPageTop.on('click', function () {
-        $(screlm).animate({ scrollTop: 0 }, 1000, 'easeInOutCirc');
-        return false;
-    });
+    }
+    /**
+     * observeSelectorName で指定した複数の要素(ページトップへ戻るボタン)を IntersectionObserverクラス による監視対象とする
+     *
+     * @returns {Object} observer - IntersectionObserverクラス
+     */
+    rptShow = () => {
+        // instance
+        const observer = new IntersectionObserver(this.addClass, this.options);
+        // observe
+        for (const selector of this.observeElmsArray) {
+            observer.observe(selector);
+        }
+        return observer;
+    }
 };
+
+module.exports = returnPageTop;
--- src/js/app.js
+++ src/js/app.js
@@ -1,158 +1,59 @@
 import $ from 'jquery';
-import 'bootstrap-honoka/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import 'jquery.easing/jquery.easing';
 import plugins from './_plugins/_plugins';
 
-//ユーザーエージェントからスクロールを実行する対象を判定
-const scrollElm = () => {
-    if ('scrollingElement' in document) {
-        return document.scrollingElement;
-    }
-    if (navigator.userAgent.indexOf('WebKit') !== -1) {
-        return document.body;
-    }
-    return document.documentElement;
-};
 
-//ページ内スクロール
-const pageScroll = (screlm) => {
-    let navbarHeight = 0;
-    if (typeof plugins.fixedanchor_js === 'function') {
-        navbarHeight = plugins.fixedanchor_js();
-    }
-    //ナビゲーションバー
-    const $navbar = $('#navbar');
-    //ブランド名とドロップダウンコンポーネント以外のナビゲーションリスト
-    $navbar.find('.navbar-brand, .nav-item:not(.dropdown) a, .dropdown-item').on('click', function (e) {
-        const speed = 1000;
-        const classFixedAnchor = 'fixed_anchor';
-        const dataFixedAnchor = 'data-fixedanchor';
-        let href = $(this).attr('href');
-        let linkPath = '';
-        let currentPath = '';
-        let targetID = '';
-
-        if (href.indexOf('//') !== -1) {
-            //絶対パスのリンクの場合
-            linkPath = href.slice(href.indexOf('//') + 1); //`href`の値を、最初の`//`以降(ドメイン名以降全て)の文字列とする
-            currentPath = `${location.host}${location.pathname}`; //`location.host`はポート番号も含む
-        } else {
-            //相対パスのリンクの場合
-            if (/^(\/)((.*)\.html)?/.test(href)) {
-                //ルートディレクトリからの参照(`/ususama.html`または`/`)
-                linkPath = RegExp.$2; //hrefの値を、最後の`/`以降(ドメインのルートディレクトリ以降)、`.html`までの文字列(アンカーやパラメータは除外)とする(正規表現の後方参照)
-                currentPath = location.pathname.slice(location.pathname.lastIndexOf('/') + 1); //sliceに文字列の長さ以上の数値を与えると空文字列になるので今回はOK(想定は`/`のみの場合)
-            } else {
-                if (/^((\.\.\/)+)((.*)\.html)?/.test(href)) {
-                    //先祖ディレクトリへの参照(`../ususama.html`や`../../../`)
-                    linkPath = RegExp.$3; //hrefの値を、最後の`../`以降(戻りきった先祖ディレクトリ以降)、`.html`までの文字列(アンカーやパラメータは除外)とする(正規表現の後方参照)
-                    currentPath = location.pathname.slice(location.pathname.lastIndexOf('/') + 1);
-                    let currentDirectory = '';
-                    let recursivePath = location.pathname.slice(0, location.pathname.lastIndexOf('/') + 1);
-                    const parentLength = (RegExp.$1.match(/(\.\.\/)/g) || []).length;
-                    for (let i = 0; i < parentLength; i++) {
-                        currentDirectory = recursivePath.slice(0, recursivePath.lastIndexOf('/'));
-                        currentDirectory = currentDirectory.slice(currentDirectory.lastIndexOf('/') + 1);
-                        currentPath = `${currentDirectory}/${currentPath}`;
-                        recursivePath = recursivePath.slice(0, recursivePath.lastIndexOf(currentDirectory));
-                    }
-                } else if (/^(\.\/)?(.*\/)$/.test(href)) {
-                    //サブディレクトリ(下層ディレクトリ)への参照(`./contact/`, `contact/`など)
-                    linkPath = RegExp.$2; //hrefの値を、最後の`/`までの文字列とする
-                    const fullPath = location.pathname.slice(location.pathname.indexOf('/') + 1);
-                    currentPath = fullPath;
-                } else {
-                    //現在ディレクトリへの参照(`./ususama.html`または`ususama.html`または`./kiribi/ususama.html`、 `./../`など先祖ディレクトリへの参照と混合することは考慮していない)
-                    linkPath = href.slice(href.indexOf('/') + 1); //hrefの値を、最後の`/`以降(現在のディレクトリ以降)の文字列とする
-                    if (href.indexOf('#') !== -1) {
-                        //`href`の値にアンカーリンクを含む場合
-                        linkPath = linkPath.split('#')[0]; //`.html`までの文字列(アンカーやパラメータは除外)に分解
-                    }
-
-                    const fullPath = location.pathname.slice(location.pathname.indexOf('/') + 1);
-                    currentPath = fullPath;
-                    let recursivePath = currentPath.slice(0, location.pathname.lastIndexOf('/'));
-                    for (let i = 0; i < (fullPath.match(/(\/)/g) || []).length; i++) {
-                        if (currentPath.indexOf(recursivePath.slice(0, recursivePath.indexOf('/'))) === 0) {
-                            currentPath = currentPath.slice(recursivePath.indexOf('/') + 1);
-                            recursivePath = recursivePath.slice(recursivePath.indexOf('/') + 1);
-                        }
-                    }
-                }
+const hamburgerClose = (e) => {
+    const navbar = document.querySelector('#navbar');
+    const navBarListID = 'navbarList';
+    if (
+        typeof e.currentTarget.closest(`#${navBarListID}`) !== 'undefined'
+         && e.currentTarget.closest(`#${navBarListID}`) !== null
+    ) {
+        let breakpoint = 0;
+        if (/(^|\s)navbar-expand-(\S*)/g.test(navbar.className)) {
+            switch (RegExp.$2) {
+                case 'sm':
+                    breakpoint = 576;
+                    break;
+                case 'md':
+                    breakpoint = 768;
+                    break;
+                case 'lg':
+                    breakpoint = 992;
+                    break;
+                case 'xl':
+                    breakpoint = 1200;
+                    break;
+                case 'xxl':
+                    breakpoint = 1400;
+                    break;
+                default:
+                    breakpoint = 0;
+                    break;
             }
         }
-
-        if (linkPath === currentPath) {
-            //`linkPath`(`href`のリンク先、アンカーは除外)の値と`currentPath`(現在ページのURL)が同じ場合
-            if (href.indexOf('#') !== -1) {
-                //`href`の値にアンカーリンクを含む場合、アンカーリンクを抽出
-                targetID = `#${href.split('#')[1]}`;
-            } else {
-                //アンカーリンクがない場合は`html`タグをターゲットにする
-                targetID = 'html';
-            }
-            let $target = $(targetID);
-            let position = Math.ceil($target.offset().top);
-            //$targetがクラス`fixed_anchor`を持っていない場合は差し引きを調整する
-            if (!$target.hasClass(classFixedAnchor) || $('body').attr(dataFixedAnchor) === 'false') {
-                position = position - navbarHeight;
-            }
-            $(screlm).animate({ scrollTop: position }, speed, 'easeInOutCirc');
-            const navBarListID = 'navbarList';
-            if ($(e.currentTarget).closest('#' + navBarListID).length > 0) {
-                let breakpoint = 0;
-                if (/(^|\s)navbar-expand-(\S*)/g.test($navbar.children('.navbar').prop('class'))) {
-                    switch (RegExp.$2) {
-                        case 'sm':
-                            breakpoint = 576;
-                            break;
-                        case 'md':
-                            breakpoint = 768;
-                            break;
-                        case 'lg':
-                            breakpoint = 992;
-                            break;
-                        case 'xl':
-                            breakpoint = 1200;
-                            break;
-                        default:
-                            breakpoint = 0;
-                            break;
-                    }
+        if (window.innerWidth < breakpoint) {
+            // ブレークポイント未満の幅のとき
+            const navbarTogglers = navbar.querySelectorAll(`.navbar-toggler[data-bs-target="#${navBarListID}"]`);
+            navbarTogglers.forEach((navbarToggler) => {
+                if(!navbarToggler.classList.contains('collapsed')) {
+                    // 現在の表示がハンバーガーメニューの場合、
+                    // 移動したらハンバーガーを折りたたむ
+                    navbarToggler.dispatchEvent(new Event('click'));
                 }
-                //ナビゲーションバー
-                if ($(window).outerWidth() < breakpoint && !$navbar.find('.navbar-toggler[data-target="#' + navBarListID + '"]').hasClass('collapsed')) {
-                    //現在の表示がハンバーガーメニューの場合
-                    $navbar.find('.navbar-toggler[data-target="#' + navBarListID + '"]').trigger('click'); //移動したらハンバーガーを折りたたむ
-                } else if ($(e.currentTarget).hasClass('dropdown-item') && $(e.currentTarget).closest('.dropdown').hasClass('show')) {
-                    //現在の表示がハンバーガーメニューではなく、ドロップダウン内のメニューをクリックした場合
-                    $(e.currentTarget).closest('.dropdown').trigger('click'); //移動したらドロップダウンを折りたたむ
+                else if(
+                    e.currentTarget.classList.contains('dropdown-item')
+                     && e.currentTarget.closest('.dropdown').classList.contains('show')
+                ) {
+                    // 現在の表示がハンバーガーメニューではなく、ドロップダウン内のメニューをクリックした場合
+                    // 移動したらドロップダウンを折りたたむ
+                    e.currentTarget.closest('.dropdown').dispatchEvent(new Event('click'));
                 }
-            }
-            return false;
-        } else {
-            //`href`の値とリンク先のURLが異なる場合は、通常のaタグの動作に戻す
-            return true;
+            });
         }
-    });
-
-    //一般
-    //ナビゲーションバーは除く
-    $('a[href^="#"]:not(.nav-link)').on('click', function () {
-        const speed = 1000;
-        const classFixedAnchor = 'fixed_anchor';
-        const dataFixedAnchor = 'data-fixedanchor';
-        let href = $(this).attr('href');
-        let targetID = href === '#' || href === '' ? 'html' : href; //リンク先が#か空だったらhtmlに
-        let $target = $(targetID);
-        let position = Math.ceil($target.offset().top);
-        //$targetがクラス`fixed_anchor`を持っていない場合は差し引きを調整する
-        if (!$target.hasClass(classFixedAnchor) || $('body').attr(dataFixedAnchor) === 'false') {
-            position = position - navbarHeight;
-        }
-        $(screlm).animate({ scrollTop: position }, speed, 'easeInOutCirc');
-        return false;
-    });
+    }
 };
 
 $(() => {
@@ -161,15 +62,17 @@ $(() => {
         plugins.ios();
     }
 
-    //スクロール対象を取得
-    const screlm = scrollElm();
     //ページトップへ戻る
     if (typeof plugins.pagetop === 'function') {
-        plugins.pagetop(screlm);
+        new plugins.pagetop();
     }
 
-    //ページ内スクロール
-    pageScroll(screlm);
+    // ブランド名とドロップダウンコンポーネント 「以外」 のナビゲーションリスト
+    const navLinks = document.querySelectorAll('.navbar-brand, .nav-item:not(.dropdown) a, .dropdown-item');
+    navLinks.forEach((navLink) => {
+        // 第二引数に hamburgerClose(navLink) のように関数を渡すと、関数の実行結果が渡されてしまうのでNG。名前だけにする。逆に e でイベントターゲットは拾えるので引数を与える必要はない
+        navLink.addEventListener('click', hamburgerClose, false);
+    });
 
     //slick
     if (typeof plugins.slick === 'function') {
